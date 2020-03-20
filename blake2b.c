3313ef5031a31501262624720dbd1167b1079820
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-03-20 20:26:29 +0900

    fix: make blake2b

--- blake2b.c
+++ blake2b.c
@@ -31,8 +31,8 @@
 #include <stdint.h>
 #include <string.h>
 
-#include "crypto/sph_types.h"
-#include "utils/sysendian.h"
+#include "sph_types.h"
+#include "sysendian.h"
 #include "blake2b.h"
 
 // Cyclic right rotation.
